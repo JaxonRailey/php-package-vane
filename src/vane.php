042033b9eee3107c955367ca9b2bfa4d6980c679
JaxonRailey <96697458+JaxonRailey@users.noreply.github.com>
2023-04-07 21:05:31 +0200

    first commit

---
+++ src/vane.php
@@ -0,0 +1,234 @@
+<?php
+
+namespace JaxonRailey;
+
+class Vane {
+
+    protected string $file;
+    protected array  $data     = [];
+    protected array  $original = [];
+    protected array  $fields   = [];
+    protected bool   $filtered = false;
+
+
+    /**
+     * Set select statement
+     *
+     * @param mixed $fields (optional)
+     *
+     * @return self
+     */
+
+    public function select(...$fields) :self {
+
+        $this->fields = $fields;
+
+        if (in_array('*', $this->fields)) {
+            $this->fields = [];
+        }
+
+        return $this;
+    }
+
+
+    /**
+     * Set from statement
+     *
+     * @param string $file
+     *
+     * @return self
+     */
+
+    public function from(string $file) :self {
+
+        $file = $file . '.json';
+
+        if (is_file($file)) {
+            $json = file_get_contents($file);
+            $json = empty(trim($json)) ? '[]' : $json;
+            $this->data = json_decode($json, true);
+            $this->original = $this->data;
+        }
+
+        $this->file = $file;
+
+        return $this;
+    }
+
+    /**
+     * Set where statement
+     *
+     * @param string $field
+     * @param string $condition (optional)
+     * @param mixed $value (optional)
+     *
+     * @return self
+     */
+
+    public function where(string $field, string $condition = null, mixed $value = null) :self {
+
+        $this->filtered = true;
+
+        $where = [$field, 'like', ''];
+
+        if ($condition) {
+            $where = [$field, '=', $condition];
+        }
+
+        if (isset($value)) {
+            $where = [$field, $condition, $value];
+        }
+
+        $this->data = array_filter($this->data, function ($row) use ($where) {
+            [$field, $condition, $value] = $where;
+
+            $item = $row;
+            foreach (explode('.', $field) as $key) {
+                $item = $item[$key] ?? null;
+                if (!isset($item)) {
+                    return false;
+                }
+            }
+
+            return $this->compare($item, $condition, $value);
+        });
+
+        return $this;
+    }
+
+
+    /**
+     * Get all rows of results
+     *
+     * @return array
+     */
+
+    public function rows() :array {
+
+        if (!$this->fields) {
+            return $this->data;
+        }
+
+        $results = [];
+        foreach ($this->fields as $fields) {
+            foreach ($this->data as $index => $row) {
+                $keys  = explode('.', $fields);
+                $value = null;
+
+                foreach ($keys as $key) {
+                    if (!isset($row[$key])) {
+                        $value = null; break;
+                    }
+                    $row   = $row[$key];
+                    $value = $row;
+                }
+
+                if ($value) {
+                    $current = &$results[$index];
+                    foreach ($keys as $key) {
+                        $current = &$current[$key];
+                    }
+                    $current = $value;
+                }
+            }
+        }
+
+        return $results;
+    }
+
+
+    /**
+     * Set save/insert statement, if filtered update, then append new rows
+     *
+     * @param array|object $data
+     *
+     * @return bool
+     */
+
+    public function save(array|object $data) :bool {
+
+        $changed = false;
+
+        // if is un update
+        if ($this->filtered) {
+            $changed = true;
+            foreach ($this->data as $index => $row) {
+                $this->data[$index] = array_merge($row, $data);
+            }
+
+            $ids = array_intersect_key($this->original, $this->data);
+            $this->original = array_replace($this->original, $ids, $this->data);
+        }
+
+        // if is new massive insert
+        if (!$this->filtered && is_array($data) && is_array($data[0])) {
+            $changed = true;
+            foreach ($data as $row) {
+                $this->original[uniqid()] = $row;
+            }
+        }
+
+        // if is single array insert
+        if (!$this->filtered && is_array($data) && !is_array($data[0])) {
+            $changed = true;
+            $this->original[uniqid()] = $data;
+        }
+
+        // if is single object insert
+        if (!$this->filtered && is_object($data)) {
+            $changed = true;
+            $this->original[uniqid()] = json_decode(json_encode($data), true);
+        }
+
+        if ($changed) {
+            $json = json_encode($this->original, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            file_put_contents($this->file, $json);
+        }
+
+        $this->data     = [];
+        $this->original = [];
+        $this->fields   = [];
+        $this->filtered = false;
+
+        return true;
+    }
+
+
+    /**
+     * Set delete statement
+     *
+     * @return bool
+     */
+
+    public function delete() :bool {
+
+        if ($this->filtered) {
+            $this->original = array_diff_key($this->original, $this->data);
+        }
+
+        $json = json_encode($this->original, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+        file_put_contents($this->file, $json);
+
+        $this->data     = [];
+        $this->original = [];
+        $this->fields   = [];
+        $this->filtered = false;
+
+        return true;
+    }
+
+    protected function compare(mixed $value, string $condition, mixed $compare) {
+
+        return match(strtolower($condition)) {
+            '='     => $value == $compare,
+            '!='    => $value != $compare,
+            '>'     => $value  > $compare,
+            '>='    => $value >= $compare,
+            '<'     => $value  < $compare,
+            '<='    => $value <= $compare,
+            'like'  => str_contains($value, $compare),
+            default => false
+        };
+    }
+}
