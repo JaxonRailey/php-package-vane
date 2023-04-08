c25187f701236274495e5d8da081550818b1d633
JaxonRailey <96697458+JaxonRailey@users.noreply.github.com>
2023-04-08 23:12:46 +0200

    Update vane.php

--- src/vane.php
+++ src/vane.php
@@ -32,6 +32,20 @@ class Vane {
 
 
     /**
+     * Get single row by id
+     *
+     * @param string $id
+     *
+     * @return array|bool
+     */
+
+    public function id(string $id) :array|bool {
+
+        return isset($this->data[$id]) ? $this->data[$id] : false;
+    }
+
+
+    /**
      * Set from statement
      *
      * @param string $file
@@ -55,6 +69,7 @@ class Vane {
         return $this;
     }
 
+
     /**
      * Set where statement
      *
@@ -65,7 +80,7 @@ class Vane {
      * @return self
      */
 
-    public function where(string $field, string $condition = null, mixed $value = null) :self {
+     public function where(string $field, string $condition = null, mixed $value = null) :self {
 
         $this->filtered = true;
 
@@ -91,6 +106,105 @@ class Vane {
             }
 
             return $this->compare($item, $condition, $value);
+        });
+
+        return $this;
+    }
+
+
+    /**
+     * Set exists statement
+     *
+     * @param string $field
+     * @param bool $positive (optional)
+     *
+     * @return self
+     */
+
+    public function exists(string $field, bool $positive = true) :self {
+
+        $this->filtered = true;
+
+        $where = [$field, $positive];
+
+        $this->data = array_filter($this->data, function ($row) use ($where) {
+            [$field, $positive] = $where;
+            return $positive ? isset($row[$field]) : !isset($row[$field]);
+        });
+
+        return $this;
+    }
+
+
+    /**
+     * Set contains statement
+     *
+     * @param string $field
+     * @param mixed $value (optional)
+     * @param bool $positive (optional)
+     *
+     * @return self
+     */
+
+    public function contains(string $field, mixed $value = null, bool $positive = true) :self {
+
+        $this->filtered = true;
+
+        $where = [$field, $value, $positive];
+
+        $this->data = array_filter($this->data, function ($row) use ($where) {
+            [$field, $value, $positive] = $where;
+            return $positive ? in_array($value, $row[$field]) : !in_array($value, $row[$field]);
+        });
+
+        return $this;
+    }
+
+
+    /**
+     * Set counter statement
+     *
+     * @param string $field
+     * @param string $condition (optional)
+     * @param mixed $value (optional)
+     *
+     * @return self
+     */
+
+     public function counter(string $field, string $condition = null, mixed $value = null) :self {
+
+        $this->filtered = true;
+
+        $where = [$field, '=', ''];
+
+        if ($condition) {
+            if ($condition == 'like') {
+                $condition = '=';
+            }
+
+            $where = [$field, '=', $condition];
+        }
+
+        if (isset($value)) {
+            $where = [$field, $condition, $value];
+        }
+
+        $this->data = array_filter($this->data, function ($row) use ($where) {
+            [$field, $condition, $value] = $where;
+
+            $item = $row;
+            foreach (explode('.', $field) as $key) {
+                $item = $item[$key] ?? null;
+                if (!isset($item)) {
+                    return false;
+                }
+            }
+
+            if (!is_array($item)) {
+                return false;
+            }
+
+            return $this->compare(count($item), $condition, $value);
         });
 
         return $this;
@@ -217,6 +331,26 @@ class Vane {
 
         return true;
     }
+
+
+    /**
+     * Set truncate statement
+     *
+     * @return bool
+     */
+
+     public function truncate() :bool {
+
+        file_put_contents($this->file, '[]');
+
+        $this->data     = [];
+        $this->original = [];
+        $this->fields   = [];
+        $this->filtered = false;
+
+        return true;
+    }
+
 
     protected function compare(mixed $value, string $condition, mixed $compare) {
 
